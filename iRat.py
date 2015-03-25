f7bad6dee06d919fbb8bda88cc289fbf14bdbbe0
Swarvanu Sengupta <swarvanusg@gmail.com>
2015-03-25 11:55:08 +0530

    Added Support For recursive Folder Search

--- iRat.py
+++ iRat.py
@@ -8,6 +8,7 @@ directory = "."
 # Lets have a Walk in subdirectory of Current dir
 for root, dirs, files in os.walk(directory):
   print "Drilling directory: " + directory
+  fileExist = False
   for fileFullName in files:
     fileName, fileExtension = os.path.splitext(fileFullName)
     # Check if its a directory came here to trouble us
@@ -16,7 +17,8 @@ for root, dirs, files in os.walk(directory):
       continue;
     if(fileExtension == ".mp3"):
       tag = eyeD3.Tag()
-      tag.link(fileFullName)
+      old_file_path = root + "/" + fileFullName
+      tag.link(old_file_path)
       trackArtist = tag.getArtist()
       trackTitle = tag.getTitle()
       trackAlbum = tag.getAlbum()
@@ -26,14 +28,23 @@ for root, dirs, files in os.walk(directory):
         print "Bingo !! File Generated name is same as filename for : " + fileFullName
         continue;
       # check if file already exist
-      isDuplicate = os.path.isfile(newFileFullName)
+      new_file_path = root + "/" + newFileFullName
+      isDuplicate = os.path.isfile(new_file_path)
       duplicateNo = 1
       while(isDuplicate):
         print "File already exist, Duplicate File :" + newFileFullName
         # Try to assign new name with Duplicate No
-        duplicateNo = 1
         newFileFullName = trackTitle + "-" + trackArtist + "_" + str(duplicateNo) + ".mp3"
-        isDuplicate = os.path.isfile(newFileFullName)
+        if(newFileFullName == fileFullName):
+          print "Bingo !! File Generated name is same as filename for : " + fileFullName
+          fileExist = True
+          break;
+        new_file_path = root + "/" + newFileFullName
+        isDuplicate = os.path.isfile(new_file_path)
+        duplicateNo = duplicateNo + 1
+      if fileExist:
+        continue;
       count = count + 1
-      print str(count) + "> Renaming File : " + fileFullName + " to : " + newFileFullName        
-      os.rename(fileFullName, newFileFullName) 
+      print str(count) + "> Renaming File : " + fileFullName + " to : " + newFileFullName
+      new_file_path = root + "/" + newFileFullName
+      os.rename(old_file_path, new_file_path) 
